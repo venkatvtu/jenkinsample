a8634a6d840117969d7586c41853315227aafe88
venkatvtu <venkatvtu1398@gmail.com>
2021-06-28 10:08:17 +0530

    jenkin commit

---
+++ src/test/java/sample2/SampleTest2.java
@@ -0,0 +1,37 @@
+package sample2;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.io.FileHandler;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class SampleTest2 {
+	WebDriver wd;
+	@Test
+	
+	public void login() throws IOException
+	{
+		//System.setProperty("webdriver.chrome.driver", "C:\\vcentry\\batch167\\A17_WebDriverTest\\browser\\chromedriver.exe");
+		WebDriverManager.chromedriver().setup();
+		wd =  new ChromeDriver();
+		wd.manage().window().maximize();
+		wd.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		wd.get("https://www.google.com/");
+		File src = ((TakesScreenshot)wd).getScreenshotAs(OutputType.FILE);
+		FileHandler.copy(src, new File("C:\\vcentry\\batch167\\jenkinTest\\screenshot\\google.png"));
+		int i =1/0;
+	
+	}
+	
+
+	
+	
+}
